ea576327fc88e305b53db81706cde003621c9c2d
redcc-c <ckghdghd6635@gmail.com>
2025-12-02 05:24:55 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvzXKENAbGwf0e_V-A0LiRAbknrXuTIdU",
+  authDomain: "miniproject-380f6.firebaseapp.com",
+  projectId: "miniproject-380f6",
+  storageBucket: "miniproject-380f6.firebasestorage.app",
+  messagingSenderId: "3412525646",
+  appId: "1:3412525646:web:7e39237267492fa709791f",
+  measurementId: "G-VYLCRSTKJW"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("백그라운드 메시지:", payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
