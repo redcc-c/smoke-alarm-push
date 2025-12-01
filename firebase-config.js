f494ee95a7b587d607db76136a3aafe8723f8c03
redcc-c <ckghdghd6635@gmail.com>
2025-12-02 07:34:47 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
-// firebase-config.js
-// 네 Firebase 프로젝트 설정
+// Firebase 설정 파일
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvzXKENAbGwf0e_V-A0LiRAbknrXuTIdU",
@@ -8,8 +7,7 @@ const firebaseConfig = {
   storageBucket: "miniproject-380f6.firebasestorage.app",
   messagingSenderId: "3412525646",
   appId: "1:3412525646:web:7e39237267492fa709791f",
-  measurementId: "G-VYLCRSTKJW",
+  measurementId: "G-VYLCRSTKJW"
 };
 
-// 전역 firebase 초기화
 firebase.initializeApp(firebaseConfig);
